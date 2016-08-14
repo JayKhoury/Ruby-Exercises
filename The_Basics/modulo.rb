6b610d4f2b00d49d3b6f6463043aac5f3d6500ce
Jay Khoury <kyttcar@gmail.com>
2016-08-14 12:28:09 -0400

    Pushing the second exercise in TheBasics chapter

---
+++ The_Basics/modulo.rb
@@ -0,0 +1,6 @@
+a = 2030
+
+puts a/1000  # Finding the digit in the 1000's place
+puts a%1000/100  # Finding the digit in the 100's place
+puts a%1000%100/10 #Finding the digit in the 10th place
+puts a%1000%100%10 # Finding the digit in the one place

97d548ce5e4861718470c93b6303b1d88731b3e3
Jay Khoury <kyttcar@gmail.com>
2016-08-14 21:47:32 -0400

    updating the third question within The Basics chapter

---
+++ The_Basics/movies.rb
@@ -0,0 +1,6 @@
+movies = {"Jaws" => 1974, "The Matrix" => 1999, "The Godfather" => 1974, "citizen Kane" => 2011}
+
+puts movies["Jaws"]
+puts movies ["The Matrix"]
+puts movies["The Godfather"]
+puts movies["citizen Kane"]

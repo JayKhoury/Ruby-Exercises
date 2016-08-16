36268eef5a872d322b8c7cd73f46a10be6f48a7e
Jay Khoury <kyttcar@gmail.com>
2016-08-15 21:27:02 -0400

    Adding first and last file

---
+++ Variables/first_last.rb
@@ -0,0 +1,11 @@
+puts " Please enter your first name here :"
+first_name = gets.chomp
+
+puts " Please enter your last name here :"
+last_name =gets.chomp
+
+puts " This is your fulll name : #{first_name} #{last_name}"
+
+#or
+
+puts " This is your full name :" + first_name + " " + last_name

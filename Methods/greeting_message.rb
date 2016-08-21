220d675607ee1efb21bdea5a5b17d2d4a88df31a
Jay Khoury <kyttcar@gmail.com>
2016-08-20 22:16:35 -0400

    Solution for first question for the Methods chapter

---
+++ Methods/greeting_message.rb
@@ -0,0 +1,11 @@
+def greeting(name)
+
+"Hello #{name} and welcome to the first methods example"
+
+# or
+
+"Hello" + " " + name + " and welcome to the first methods example"
+
+end
+
+puts greeting("jay")

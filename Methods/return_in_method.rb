a4c48b672b9e1d712d6ef40c19fafc57f8fd61fb
Jay Khoury <kyttcar@gmail.com>
2016-08-22 18:11:47 -0400

    4th exercise inside the Methods chapter

---
+++ Methods/return_in_method.rb
@@ -0,0 +1,17 @@
+# What will be the output of the following method ?
+
+
+def scream(words)
+
+	words = words + "!!!"
+
+	return 
+
+	puts words
+
+end
+
+scream("jay")
+
+
+# The outcome is nothing because return ends the method 

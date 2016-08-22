8f99b100e4b8c50f1bdcf05a40e6169c68ad8fc8
Jay Khoury <kyttcar@gmail.com>
2016-08-22 18:25:50 -0400

    add 5th exercise in the Method Chapter

---
+++ Methods/method_without_return.rb
@@ -0,0 +1,14 @@
+# Change the previous method in the file labeled return_in_method.rb
+# so that it prints a word instead of printing nothing
+
+# 
+
+def scream(words)
+
+	words = words + "!!!"
+
+    puts words
+
+end
+
+scream("jay")

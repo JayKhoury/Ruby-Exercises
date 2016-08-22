46844490a7c8cf60b330162b57b410930597ba88
Jay Khoury <kyttcar@gmail.com>
2016-08-22 18:39:41 -0400

    Answer to question within the Methods chapter related to reading errors

---
+++ Methods/reading_errors.rb
@@ -0,0 +1,10 @@
+Argument error : wrong number of arguments ( 1 for 2)
+
+from (irb):1:in 'calculate_product'
+
+
+
+
+answer :
+
+The method calculate_product takes two arguments but only one argument was provided

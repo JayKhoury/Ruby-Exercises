83dd99e27b2db3be4ec9f39f483f97747376e9d4
Jay Khoury <kyttcar@gmail.com>
2016-08-25 20:29:14 -0400

    First exercise in Control FLow chapter

---
+++ FlowControl/exercise1-truefalse.rb
@@ -0,0 +1,19 @@
+# Write wether the following expressions evaluate 
+# to true or false
+
+puts (32*4) >= 129  # >>>>>false
+
+
+puts false != !true # >>>>>>>false
+
+puts true == 4  # >>>>> False , because
+#TRUE does NOT equate to 4 or vice versa. 
+
+puts false == (847=='847') # >>>> TRUE , since 847 is an integer that does not 
+# equate to a string. So that expression is false.
+# But false equates to false. So that comparison is true, hence
+# The whole expression is true.
+
+ (!true || (!(100/5 == 20) || ((328/4) == 82)) || false
+
+#  This should evaluate to true

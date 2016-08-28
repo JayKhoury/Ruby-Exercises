913802a734427fb9782fc09679db851fb554d463
Jay Khoury <kyttcar@gmail.com>
2016-08-28 09:03:27 -0400

    6th exercise in FlowCotrol chapter

---
+++ FlowControl/exercise6-error.rb
@@ -0,0 +1,17 @@
+# Find out why this method gets an error message and correct it so that
+# the methods prints properly
+
+def equal_to_four(x)
+
+	if x == 4
+		puts " It's equal to 4 ! it's equal to 4 !"
+
+	else
+		puts " It's not equal to 4 !"
+
+	end
+
+	puts equal_to_four(44)
+
+
+	# We need to put an end to the if statement and an end to the method

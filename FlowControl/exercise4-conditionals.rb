ffcf13bdae399b524a3d04236ea7b773cbbc7935
Jay Khoury <kyttcar@gmail.com>
2016-08-27 22:00:23 -0400

    4th exercise in FlowControl chapter

---
+++ FlowControl/exercise4-conditionals.rb
@@ -0,0 +1,40 @@
+# What would this code print into ?
+
+'4' == 4 ? puts("TRUE") : puts("FALSE")
+
+# The Answer is FALSE !
+
+# What would this code print into ?
+
+x = 2
+if ((x*3)/2) == ( 4 + 4 - x - 3)
+
+	puts " This is the right answer !!"
+
+else
+	puts " This is obviously NOT the right answer !"
+
+end
+
+
+# What would thic code print ?
+
+y = 9
+x = 10
+
+if (x+1) <= y
+	puts " Hoorray ! This is it !"
+
+elsif (x+1) >= y
+	puts " I did it again !"
+
+elsif (y+1) ==  x
+	puts " I hit the jackpot !"
+
+
+else
+	puts " This exercise is so basic "
+
+end
+
+# Answer is : I did it again !

fba9352a837e9a5116065cc92a4cc4925a837b62
Jay Khoury <kyttcar@gmail.com>
2016-09-06 18:13:25 -0400

    Commiting the 6th exrcise in the Arrrays chapter

---
+++ Arrays/exercise6.rb
@@ -0,0 +1,7 @@
+# How to change the following string in this array 
+# arr = ["john", "joseph", "Simone", "Ashley"]
+
+arr[3] = "Erica"
+
+# You cannot put arr["Ashley"] = "Erica", since you can only set the value
+# of an array with an integer and not with a string .

abc1e369275c486334e002a550e8c0ece577a3a0
Jay Khoury <kyttcar@gmail.com>
2016-09-06 17:57:45 -0400

    adding the 4th exercise in Arrays chater

---
+++ Arrays/exercise4.rb
@@ -0,0 +1,9 @@
+# What do the following methds return ?
+
+arr = [1,2,3,4,5]
+
+arr.index(2) # >>>>>returns the idex of the number 2 , which is 1
+
+arr.index[3] # >>>>>>returns an error since you cannot put a bracket next to a method
+
+arr[4] >>> returns the number 5, since it is in index 4 

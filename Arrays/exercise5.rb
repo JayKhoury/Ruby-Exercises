b63f7e65df736f070004c8fd8309be6b60b5c41d
Jay Khoury <kyttcar@gmail.com>
2016-09-06 18:03:55 -0400

    Adding the 5th exercise of Array chapter

---
+++ Arrays/exercise5.rb
@@ -0,0 +1,9 @@
+# What is the value of a,b and c in the following program ?
+
+string = " Welcome to America! "
+
+a = string[6] # >>>>> returns e, because e is the six index
+
+b = string[11] # >>>>> returns A because A is in the 11 th posisiton, including the space
+
+c = string[19] # returns nil because the 19th position is empty
